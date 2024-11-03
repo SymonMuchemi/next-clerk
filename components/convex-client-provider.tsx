3056749e8ce731dc39fce9996cadf5beb0a3598c
symonmuchemi <simonkenya846@gmail.com>
2024-11-04 01:31:10 +0300

    feat: sets up convex with clerk authentication
    replaces ConvexClientProvider with ConvexProviderWithClerk to access auth in convex functions

--- components/convex-client-provider.tsx
+++ components/convex-client-provider.tsx
@@ -1,8 +1,14 @@
 import { ReactNode } from 'react'
-import { ConvexReactClient, ConvexProvider } from 'convex/react'
+import { useAuth } from '@clerk/nextjs'
+import { ConvexReactClient } from 'convex/react'
+import { ConvexProviderWithClerk } from 'convex/react-clerk'
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>
+  return (
+    <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+      {children}
+    </ConvexProviderWithClerk>
+  )
 }
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://model-dodo-34.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
